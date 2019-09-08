671ad50ef1ea6c9dc1d1776a1b8bb886bf573cb1
Ivan Berk <berkoivan@gmail.com>
2019-09-08 23:15:40 +0300

    add fixed header js

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+$(function() {
+  //  fixed header
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH;
+  let scrollPos = $(window).scrollTop();
+
+  $(window).on("scroll load resize", function(){
+    let introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+    if(scrollPos > introH){
+      header.addClass("fixed");
+    }else {
+      header.removeClass("fixed");
+    }
+  });
+
+  // Smooth scroll
+
+  
+
+  
+});
